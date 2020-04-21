ff9d54d3f8ed981740cb44bb44c669444383d9cb
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-21 23:06:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
+    
     @total += price*quantity
     counter = 1
     while counter <= quantity
@@ -33,7 +34,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    @previous_total
+    
     @items.pop()
     if @items = []
       @total = 0.0
